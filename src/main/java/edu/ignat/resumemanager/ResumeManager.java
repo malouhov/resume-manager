f45405189ade91506cdeaaa0d044db9c6255c03e
Ignat Malouhov <malouhov@yahoo.com>
2023-02-04 13:21:51 +0300

    Init commit

---
+++ src/main/java/edu/ignat/resumemanager/ResumeManager.java
@@ -0,0 +1,13 @@
+package edu.ignat.resumemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeManager {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResumeManager.class, args);
+	}
+
+}
---
+++ src/test/java/edu/ignat/resumemanager/ResumeManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ignat.resumemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
