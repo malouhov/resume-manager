b34d79dd0fcd334b8b61e816c4f9ae255b2b082e
Ignat Malouhov <malouhov@yahoo.com>
2023-02-06 21:42:11 +0300

    Add entities Company, Response, Tag and map them together

--- src/main/java/edu/ignat/resumemanager/model/Response.java
+++ src/main/java/edu/ignat/resumemanager/model/Response.java
@@ -1,21 +1,39 @@
 package edu.ignat.resumemanager.model;
 
-import java.time.LocalDateTime;
-import java.util.List;
+import jakarta.persistence.*;
+import lombok.Data;
 
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@Entity
+@Table(name = "responses")
 public class Response {
 
+    @Id
+    @GeneratedValue
     private long id;
 
+    @ManyToOne
+    @JoinColumn(name = "fk_company")
     private Company company;
 
     private String vacancy;
 
+    private String description;
+
     private LocalDateTime send;
 
-    private Status status;
+    @ManyToMany
+    @JoinTable(name = "response_tag",
+            joinColumns = {@JoinColumn(name = "fk_response")},
+            inverseJoinColumns = {@JoinColumn(name = "fk_tag")})
+    private Set<Tag> tags = new HashSet<>();
 
-    private List<Tag> tags;
+    @Enumerated(EnumType.ORDINAL)
+    private Status status;
 
     public enum Status {
 
@@ -26,6 +44,5 @@ public class Response {
         REJECT
 
     }
-
 
 }
--- src/main/java/edu/ignat/resumemanager/model/Tag.java
+++ src/main/java/edu/ignat/resumemanager/model/Tag.java
@@ -1,8 +1,23 @@
 package edu.ignat.resumemanager.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@Entity
+@Table(name = "tags")
 public class Tag {
 
+    @Id
+    @GeneratedValue
     private long id;
+
     private String name;
+
+    @ManyToMany(mappedBy = "tags")
+    private Set<Response> tags = new HashSet<>();
 
 }
